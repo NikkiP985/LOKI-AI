e19b986c6a941d214ac38616b8be47c4636464b7
NikkiP985 <carrollnikki836@gmail.com>
2025-06-14 09:06:30 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -91,3 +91,17 @@ class LokiApp(App):
 
 if __name__ == "__main__":
     LokiApp().run()
+    [app]
+title = LOKI
+package.name = loki
+package.domain = org.loki
+source.dir = .
+source.include_exts = py
+version = 0.1
+requirements = python3,kivy,pyjnius
+orientation = portrait
+fullscreen = 1
+
+[buildozer]
+log_level = 2
+warn_on_root = 1
